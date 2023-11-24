d72d00d0700df7aed3826c7dea969f5fd60ade68
Omid Aghaei <info@omidaghaei.ir>
2023-11-24 15:37:21 +0330

    wrap things up

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="w-screen h-screen flex items-center justify-center text-sm">
+    <div class="container rounded border bg-gray-100 p-4">
+
+      <customerAddVue @save="add" />
+      <customerListHeader />
+      <customerListEmpty v-if="customersList.length === 0" />
+      <customerListItem
+        v-for="row in customersList"
+        :key="row.Email"
+        :data="row"
+        @remove="remove(row.Email)"
+        @update="update(row.Email, $event)"
+      />
+
+    </div>
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import { useCustomers } from '@/composables/customers'
+import customerAddVue from "./components/customerAdd.vue"
+import customerListHeader from "./components/customerListHeader.vue"
+import customerListItem from './components/customerListItem.vue'
+import customerListEmpty from './components/customerListEmpty.vue'
+const customers = useCustomers()
+const customersList = customers.customersList()
+const add = function(e) { customers.add(e) }
+const remove = function(e) { customers.remove(e) }
+const update = function(e, data) { customers.update(e, data) }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
