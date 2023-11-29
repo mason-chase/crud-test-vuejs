3ac6e57d2ca332ed9e67ff0c5abed696bf218884
Alireza Safari <alireza.safaree@gmail.com>
2023-11-29 22:55:15 +0330

    core(vite.config.ts): adds vite-plugin-federation for microfrontend & seprates app logic from customer logic

---
+++ host/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import federation from "@originjs/vite-plugin-federation";
+
+const path = require("path")
+// https://vitejs.dev/config/
+export default defineConfig({
+  preview: {
+    port: 3000
+  },
+  plugins: [
+    vue(),
+    federation({
+      name: "host",  //app name
+      remotes: {
+        customer: "http://localhost:3001/assets/customer.js",  //remote path containing the port configured on remote side, the build path, and the filename also configured on the remote side
+      },
+      shared: ["vue"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+})
