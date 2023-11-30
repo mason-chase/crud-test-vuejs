d2a5b70ce2c173e1386d6524e4d7dd1823345649
Alireza Safari <alireza.safaree@gmail.com>
2023-11-30 23:20:58 +0330

    refactor(customer-store): Refactors customer store and adds docs for store methods

--- customer/src/infrastructure/store/customer.ts
+++ customer/src/infrastructure/store/customer.ts
@@ -8,12 +8,21 @@ export const useCustomerStore = defineStore('customer', {
   }),
 
   actions: {
+    /**
+     * Clears store states.
+     */
     clear() {
       this.$reset()
     },
+    /**
+     * Creates given customer
+     */
     create(customer: Customer) {
       this.customers.push(customer)
     },
+    /**
+     * Deletes customer if possible return value indicates deletion state if possible true will be returned. 
+     */
     delete(id: string): boolean {
       const index = this.customers.findIndex((x: Customer) => x.id === id)
 
@@ -22,8 +31,10 @@ export const useCustomerStore = defineStore('customer', {
 
       this.customers.splice(index, 1)
       return true
-
     },
+    /**
+     * Updates given customer 
+     */
     update(customer: Customer): Customer {
       const index = this.customers.findIndex((x: Customer) => x.id === customer.id)
       this.customers[index] = customer
@@ -31,9 +42,15 @@ export const useCustomerStore = defineStore('customer', {
     }
   },
   getters: {
+    /**
+     * Returns all customers
+     */
     all(state) {
       return state.customers
     },
+    /**
+     * Returns customer by given id if customer is not found null will be returned.
+     */
     getById: (state) => {
       return (id: string) => {
 
