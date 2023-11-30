0f9f908964c05a2c34cfc7d68281de4f14dcb757
Alireza Safari <alireza.safaree@gmail.com>
2023-11-30 22:19:48 +0330

    test(customer-tests): [ Green Light ] for customer store getters -  implements getById and all getters

---
+++ customer/src/infrastructure/store/customer.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia'
+import { Customer } from '~/domain/customer'
+
+export const useCustomerStore = defineStore('customer', {
+
+  state: () => ({
+    customers: [] as Customer[],
+  }),
+
+  getters: {
+    all(state) {
+      return state.customers
+    },
+    getById: (state) => {
+      return (id: string) => {
+
+        const index = state.customers.findIndex(x => x.id === id)
+
+        if (index === -1)
+          return null
+
+        return state.customers[index]
+      }
+    }
+  }
+})
