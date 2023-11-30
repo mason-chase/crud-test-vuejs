619a60c0ae210862f1d01675852d0af2768ce5f1
Alireza Safari <alireza.safaree@gmail.com>
2023-11-30 21:51:15 +0330

    test(customer-tests): [ Red Light ] for Customer Model & Base Model - Creates BaseModel and extends Customer from BaseModel

--- customer/tests/domain/customer.test.ts
+++ customer/tests/domain/customer.test.ts
@@ -1,17 +1,32 @@
 import { it, describe, expectTypeOf } from 'vitest'
 import { Customer } from '~/domain/customer'
+import { BaseModel } from '~/domain/base'
 
 // According to vitest type documentation typechecking is an expiremntal future and it's only available in beta versions
 // But I didn't want to switch to a beta version therfore type test only works using typescript compiler
 // https://vitest.dev/guide/testing-types#run-typechecking
 
-describe('Customer Domain', () => {
+describe('Base Model', () => {
+
+  it('Check Customer property types', () => {
+
+    const baseModel = new BaseModel()
+
+    expectTypeOf(baseModel.id).toBeString()
+    expectTypeOf(baseModel.createdDate).toEqualTypeOf<Date>()
+  })
+
+})
+
+describe('Customer Model', () => {
 
   it('Check Customer property types', () => {
 
     const customer = new Customer()
 
-    expectTypeOf(customer.firstName).toBeString()
+    expectTypeOf(customer.id).toBeString()
+    expectTypeOf(customer.createdDate).toEqualTypeOf<Date>()
+
     expectTypeOf(customer.lastName).toBeString()
     expectTypeOf(customer.phoneNumber).toBeString()
     expectTypeOf(customer.email).toBeString()
