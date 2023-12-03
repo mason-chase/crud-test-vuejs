0b6c0f54398e3fd3216e8b741dae06cd8b262e60
Alireza Safari <alireza.safaree@gmail.com>
2023-12-03 22:51:01 +0330

    refactor(customer-e2e): adds command descriptions

--- customer/tests/e2e/support/index.ts
+++ customer/tests/e2e/support/index.ts
@@ -8,9 +8,25 @@ declare global {
        * @example cy.clickThe('addBtn')
        */
       clickThe(selector: string): Chainable<JQuery<HTMLElement>>
+      /**
+       * Check existance of a DOM element.
+       * @example cy.existsThe('addBtn')
+       */
       existsThe(selector: string): Chainable<JQuery<HTMLElement>>
+      /**
+       * Find input by selector and then type given data.
+       * @example cy.clickThe('addBtn')
+       */
       fillThe(selector: string, value: string): Chainable<JQuery<HTMLElement>>
+      /**
+       * Get DOM element by given selector.
+       * @example cy.getThe('firstName', '1')
+       */
       getThe(selector: string): Chainable<JQuery<HTMLElement>>
+      /**
+       * Check not existance of a DOM element.
+       * @example cy.notExistsThe('addBtn')
+       */
       notExistsThe(selector: string): Chainable<JQuery<HTMLElement>>
     }
   }
