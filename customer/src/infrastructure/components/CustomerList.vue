cd0acd872a6565b7e9b9511f8a6ad9863b0845f5
Alireza Safari <alireza.safaree@gmail.com>
2023-12-03 22:46:20 +0330

    test(customer-e2e): completes tests for customerList

--- customer/src/infrastructure/components/CustomerList.vue
+++ customer/src/infrastructure/components/CustomerList.vue
@@ -1,29 +1,29 @@
 <template>
   <h3 class="card title">Customers List</h3>
   <div id="customerList">
-    <div v-if="formErrors" class="formErrors">{{ formErrors }}</div>
+    <div v-if="formErrors" data-test="formErrors" class="formErrors">{{ formErrors }}</div>
     <div class="card">
       <div class="customer-list-header">
-        <button @click="addCustomer" class="button" test-id="addBtn">Add</button>
-        <button @click="deleteAll" class="button" test-id="deleteAllBtn">Delete All</button>
+        <button @click="addCustomer" class="button" data-test="addBtn">Add</button>
+        <button @click="deleteAll" class="button" data-test="deleteAllBtn">Delete All</button>
       </div>
       <div class="customer-list-body">
-        <div v-for="(customer, index) in customers" :key="index" class="customer-card" :test-id="`customerCard-${index}`">
+        <div v-for="(customer, index) in customers" :key="index" class="customer-card" :data-test="`customerCard-${index}`">
           <div class="customer-card-header">
             <div class="customer-info">
               <div class="text-primary">
-                <span class="main-info " v-if="customer.readonly">{{ customer.firstName }} {{ customer.lastName }}</span>
+                <span class="main-info" data-test="fullName" v-if="customer.readonly">{{ customer.firstName }} {{ customer.lastName }}</span>
                 <div v-else>
                   <div class="d-flex">
                     <div class="label">FirstName:</div>
                     <div class="value">
-                      <input type="text" v-model="customer.firstName" id="firstName" placeholder="FirstName">
+                      <input type="text" v-model="customer.firstName" id="firstName" data-test="firstName" placeholder="FirstName">
                     </div>
                   </div>
                   <div class="d-flex">
                     <div class="label">LastName:</div>
                     <div class="value">
-                      <input type="text" v-model="customer.lastName" id="lastName" placeholder="LastName">
+                      <input type="text" v-model="customer.lastName" id="lastName" data-test="lastName" placeholder="LastName">
                     </div>
                   </div>
                 </div>
@@ -33,29 +33,29 @@
                   <div class="label">Email:</div>
                   <div class="value">
                     <span v-if="customer.readonly">{{ customer.email }}</span>
-                    <input v-else type="email" v-model="customer.email" id="email" placeholder="Email">
+                    <input v-else type="email" v-model="customer.email" id="email" data-test="email" placeholder="Email">
                   </div>
                 </div>
                 <div class="d-flex">
                   <div class="label">Phone Number:</div>
                   <div class="value">
                     <span v-if="customer.readonly">{{ customer.phoneNumber }}</span>
-                    <input v-else type="text" v-model="customer.phoneNumber" id="phoneNumber" placeholder="Phone Number">
+                    <input v-else type="text" v-model="customer.phoneNumber" id="phoneNumber" data-test="phoneNumber" placeholder="Phone Number">
                   </div>
                 </div>
                 <div class="d-flex">
                   <div class="label">Bank Account Number:</div>
                   <div class="value">
                     <span v-if="customer.readonly">{{ customer.bankAccountNumber }}</span>
-                    <input v-else type="text" v-model="customer.bankAccountNumber" id="bankAccountNumber" placeholder="Bank Account Number">
+                    <input v-else type="text" v-model="customer.bankAccountNumber" id="bankAccountNumber" data-test="bankAccountNumber" placeholder="Bank Account Number">
                   </div>
                 </div>
               </div>
             </div>
             <div class="customer-buttons">
-              <i class="fa-solid fa-trash small-button delete" @click="deleteCustomer(customer, index)"></i>
-              <i v-if="customer.readonly" class="fa-solid fa-edit small-button" @click="editCustomer(customer)"></i>
-              <i v-else class="fa-solid fa-check small-button" @click="saveCustomer(customer, index)"></i>
+              <i class="fa-solid fa-trash small-button delete" data-test="deleteBtn" @click="deleteCustomer(customer, index)"></i>
+              <i v-if="customer.readonly" class="fa-solid fa-edit small-button" data-test="editBtn" @click="editCustomer(customer)"></i>
+              <i v-else class="fa-solid fa-check small-button" data-test="saveBtn" @click="saveCustomer(customer, index)"></i>
             </div>
           </div>
           <div class="customer-card-bottom text-secondary">
@@ -63,7 +63,7 @@
               <div class="label">Date Of Birth:</div>
               <div class="value">
                 <span v-if="customer.readonly">{{ new Date(customer.dateOfBirth).toLocaleDateString() }}</span>
-                <input v-else type="date" v-model="customer.dateOfBirth" min="1900-01-01" max="2099-12-31" id="dateOfBirth" placeholder="Date Of Birth">
+                <input v-else type="date" v-model="customer.dateOfBirth" min="1900-01-01" max="2099-12-31" id="dateOfBirth" data-test="dateOfBirth" placeholder="Date Of Birth">
               </div>
             </div>
           </div>
--- customer/tests/e2e/steps/CustomerList.ts
+++ customer/tests/e2e/steps/CustomerList.ts
@@ -1,11 +1,92 @@
 // cypress/e2e/duckduckgo.ts
 import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor"
+import { customer } from '../mock'
 
-Given("I click on the button", () => {
+function init() {
   cy.visit("http://localhost:3001/")
+}
+
+function create_customer(index = 1) {
+  cy.clickThe('addBtn')
+  cy.existsThe('customerCard-0')
+  cy.fillThe('firstName', `${customer.firstName}${index}`)
+  cy.fillThe('lastName', `${customer.lastName}${index}`)
+  cy.fillThe('email', `${index}${customer.email}`)
+  cy.fillThe('phoneNumber', `${customer.phoneNumber}`.replace(customer.phoneNumber.at(-1), index.toString()))
+  cy.fillThe('bankAccountNumber', `${customer.bankAccountNumber}`.replace(customer.bankAccountNumber.at(-1), index.toString()))
+  cy.fillThe('dateOfBirth', `2023-01-0${index}`)
+  cy.clickThe('saveBtn')
+  cy.existsThe('fullName')
+}
+
+Given("Filling empty form and then clicking on submit", () => {
+  init()
 })
 
-Then("Empty customer card should be added", () => {
-  cy.get("button[test-id=addBtn]").click()
-  cy.get("div[test-id=customerCard-0]").should('exist')
+Then("Should add 1 submited customer", () => {
+  create_customer()
+})
+
+Given("Update firstName of existing customer", () => {
+  init()
+  create_customer()
+  cy.existsThe('customerCard-0')
+})
+
+Then("Should change firstName to updated value", () => {
+  cy.clickThe('editBtn')
+  cy.fillThe('firstName', 'updated')
+  cy.clickThe('saveBtn')
+  cy.existsThe('fullName').contains('updated')
+})
+
+Given("Clicking on trash icon should delete the existing customer", () => {
+  init()
+  create_customer()
+  cy.existsThe('customerCard-0')
+})
+
+Then("Customer list should be empty after delete", () => {
+  cy.clickThe('deleteBtn')
+  cy.notExistsThe('customerCard-0')
+})
+
+
+Given("Clicking on Delete All should delete all customers", () => {
+  init()
+  create_customer(1)
+  create_customer(2)
+  create_customer(3)
+  cy.existsThe('customerCard-0')
+  cy.existsThe('customerCard-1')
+  cy.existsThe('customerCard-2')
+})
+
+Then("Customer list should be empty after delete all", () => {
+  cy.clickThe('deleteAllBtn')
+  cy.notExistsThe('customerCard-0')
+  cy.notExistsThe('customerCard-1')
+  cy.notExistsThe('customerCard-2')
+})
+
+Given("Clicking save on not validated data", () => {
+  init()
+  create_customer(1)
+  cy.existsThe('customerCard-0')
+})
+
+Then("Should display formErors", () => {
+  create_customer(1)
+  cy.existsThe('formErrors')
+})
+
+Given("Refreshing page with 1 customer", () => {
+  init()
+  create_customer()
+  cy.existsThe('customerCard-0')
+})
+
+Then("Added customers should exist", () => {
+  cy.reload()
+  cy.existsThe('customerCard-0')
 })
