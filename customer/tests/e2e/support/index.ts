88428f7b04489924225f1648ea99e7ff1b693ea0
Alireza Safari <alireza.safaree@gmail.com>
2023-12-04 09:06:38 +0330

    test(customer-e2e): fix custom commands doc strings

--- customer/tests/e2e/support/index.ts
+++ customer/tests/e2e/support/index.ts
@@ -10,22 +10,22 @@ declare global {
       clickThe(selector: string): Chainable<JQuery<HTMLElement>>
       /**
        * Check existance of a DOM element.
-       * @example cy.existsThe('addBtn')
+       * @example cy.existsThe('customerCard-0')
        */
       existsThe(selector: string): Chainable<JQuery<HTMLElement>>
       /**
        * Find input by selector and then type given data.
-       * @example cy.clickThe('addBtn')
+       * @example cy.fillThe('fistName', 'john')
        */
       fillThe(selector: string, value: string): Chainable<JQuery<HTMLElement>>
       /**
        * Get DOM element by given selector.
-       * @example cy.getThe('firstName', '1')
+       * @example cy.getThe('formErrors')
        */
       getThe(selector: string): Chainable<JQuery<HTMLElement>>
       /**
        * Check not existance of a DOM element.
-       * @example cy.notExistsThe('addBtn')
+       * @example cy.notExistsThe('formErrors')
        */
       notExistsThe(selector: string): Chainable<JQuery<HTMLElement>>
     }
